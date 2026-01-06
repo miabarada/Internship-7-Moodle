8d923a830eec9d594da2c918351dc6cadd143d4d
miabarada <mia.barada2005@gmail.com>
2026-01-06 20:06:39 +0100

    Announcement, material funcionalities added to application, courses fixed

--- Domain/DTOs/CourseDTO.cs
+++ Domain/DTOs/CourseDTO.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain.DTOs
+﻿namespace Domain.DTOs
 {
-    internal class Course
+    public class CourseDTO
     {
+        public int Id { get; init; }
+        public string? Name { get; set; }
+        public int ProfesorId { get; init; }
     }
 }
