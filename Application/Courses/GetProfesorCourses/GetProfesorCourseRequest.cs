a0f51456355ed4dca19db4b24c4f1a85aae13b60
miabarada <mia.barada2005@gmail.com>
2026-01-06 17:08:29 +0100

    course funcionalities added in application

--- Application/Courses/GetProfesorCourses/GetProfesorCourseRequest.cs
+++ Application/Courses/GetProfesorCourses/GetProfesorCourseRequest.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Application.Courses.CreateCourse
+namespace Application.Courses.GetProfesorCourses
 {
-    internal class CreateCourse
+    internal class GetProfesorCourseRequest
     {
     }
 }
